0e9dcf010df03a86e33991cfea9e60a62cd5f592
stagiaire <jeremy.perrouault@gmail.com>
2023-12-13 12:19:08 +0100

    JOUR 3 - GATEWAY

---
+++ PROJETS/gateway/src/main/java/fr/formation/gateway/CustomFilter.java
@@ -0,0 +1,20 @@
+package fr.formation.gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomFilter implements GatewayFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("OK");
+
+        chain.filter(exchange);
+
+        return Mono.empty();
+    }
+}
